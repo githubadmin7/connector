f29729b251ca3f5dbbfb42d8f1b88651b63d70dd
wechat_l10n@pactera.com <wechat_l10n@pactera.com>
2018-08-24 11:30:07 +0800

    test delete

--- json/1 - 副本.js
+++
@@ -1,4 +0,0 @@
-{
-	"download-agent-info-msg":"<p>Use a non-mobile device to download the agent.</p>",
-	"download-agent-info-msg":"<p>Use a non-mobile device to download the agent.</p>"   
-}      
